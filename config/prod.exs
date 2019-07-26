0fd577dc6fd59f910678b02a37452f7ccb40b75c
Kevin English <kenglish@monksealsoftware.com>
2019-07-26 15:57:11 -0700

    try a different host

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :babyweeks, BabyweeksWeb.Endpoint,
-  url: [scheme: "https", host: "babyweeks", port: 443],
+  url: [scheme: "https", host: "babyweeks.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
